ee5831602b1b40087ed355879bbfe557d604b399
wasmup <@wasmup>
2022-05-25 23:50:54 +0430

    show the address

--- 100/web/main.go
+++ 100/web/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	err := http.ListenAndServe(":8181", nil)
+	address := ":8181"
+	fmt.Println("Open:", address)
+	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
